b66437ba07a7b152c3dfd3a3aaa0c76e607acea8
ikarus <93844727+0xikarus@users.noreply.github.com>
2025-01-05 19:20:50 +0100

    fix footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,9 +1,22 @@
-
+import Link from 'next/link';
 
 export default function Footer() {
     return (
-        <footer className="bg-hs-blue-primary text-white p-4">
-            <p>Footer</p>
+        <footer className="border-t border-gray-200 py-4">
+            <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <ul className="flex space-x-6 text-gray-600">
+                    <li>
+                        <Link href="/articles" className="hover:text-gray-900">
+                            Articles
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/support" className="hover:text-gray-900">
+                            Support
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </footer>
-    )
+    );
 }
